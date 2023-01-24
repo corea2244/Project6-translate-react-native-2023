0a231282be4fa871b545e7a56a2a3b63f45b7843
corea2244 <59365194+corea2244@users.noreply.github.com>
2023-01-24 22:58:13 +0900

    [part2-ch6-2-i18n-js을 이용한 Localization 세팅, custom hook - useTranslati…

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import { StyleSheet, Text, View } from "react-native";
 import Button from "./src/Button";
+import { useCookie } from "./src/use-cookie";
 import { useTranslation } from "./src/use-translation";
 
 export default function App() {
   const { t, locale, setLocale } = useTranslation();
+  const { cookieKey } = useCookie();
 
   if (locale === null) return null;
 
   return (
     <View style={styles.container}>
-      <Text>{t("cookie_1")}</Text>
+      <Text>{t(cookieKey)}</Text>
       <View style={styles.buttonsContainer}>
         <Button
           onPress={() => setLocale("ko")}
